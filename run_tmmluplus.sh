38b90921fa82adc200a4a1c444406b9d0d336ce7
MarkCodering <mark.chen.sstm@gmail.com>
2025-10-25 19:14:16 +0800

    update batch size and clean up commented code in run_tmmluplus.sh

--- run_tmmluplus.sh
+++ run_tmmluplus.sh
@@ -1,12 +1,11 @@
-#lm_eval --model hf \
-#    --model_args pretrained=MarkChenX/gemma-12b-tairis-4bq \
-#    --tasks mmlu \
-#    --num_fewshot 5 \
-#    --device cuda:0 \
-#    --batch_size 16
-
 lm_eval --model hf \
     --model_args pretrained=MarkChenX/gemma-12b-tairis-4bq \
     --tasks tmmluplus \
     --device cuda:0 \
-    --batch_size 16
+    --batch_size 1
+
+lm_eval --model hf \
+    --model_args pretrained=YL-Ting/gemma-3-4b-tairis-4bq \
+    --tasks tmmluplus \
+    --device cuda:0 \
+    --batch_size 1
